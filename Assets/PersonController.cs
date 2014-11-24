bcaecb5198b43d33d3dfe2dfa53e51cb9b82e7eb
Mario Quesada <mario.quesada@socialpoint.es>
2014-11-24 03:22:20 +0100

    first commit

---
+++ Assets/PersonController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonController : MonoBehaviour
+{
+	[SerializeField]
+	float _moveSpeed = 5f;
+
+	[SerializeField]
+	float _angularSpeed = 70f;
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.position += transform.forward * _moveSpeed *Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			transform.position += -transform.forward * _moveSpeed *Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			Quaternion deltaRotation = Quaternion.AngleAxis(-_angularSpeed * Time.deltaTime, Vector3.up);
+			Quaternion nextQuat = transform.rotation * deltaRotation;
+			transform.rotation = nextQuat;
+
+			transform.rotation = nextQuat;
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			Quaternion deltaRotation = Quaternion.AngleAxis(_angularSpeed * Time.deltaTime, Vector3.up);
+			Quaternion nextQuat = transform.rotation * deltaRotation;
+			transform.rotation = nextQuat;
+			
+			transform.rotation = nextQuat;
+		}
+	}
+}
